87bf9ffab65bbcc497b98ca82112bc46987d7613
Rafa <rafaokc33@gmail.com>
2024-06-01 11:31:53 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,28 @@
 import "./Footer.css"
+import githubLogo from "../assets/github-logo.png"
+import linkedinLogo from "../assets/linkedin-logo.png"
 
 function Footer (){
   return(
     <div id="footer">
       <div id="footer-text">
         <h5>Get in Touch</h5>
-        <p></p>
+        <p>If you need solutions for your website, don't hesitate to contact us!</p>
       </div>
       <div id="footer-info">
         <div id="footer-links">
           <h5>Mahendra</h5>
-          <a href="" target="_blank"><img src="../assets/github-logo.png" alt="github"/></a>
-          <a href="" target="_blank"><img src="../assets/linkedin-logo.png" alt="linkedin"/></a>
+          <div id="footer-img">
+            <a href="" target="_blank"><img src={githubLogo} alt="github"/></a>
+            <a href="" target="_blank"><img src={linkedinLogo} alt="linkedin"/></a>
+          </div>
         </div>
         <div id="footer-links">
         <h5>Rafael</h5>
-          <a href="https://github.com/Rafa3399" target="_blank"><img src="../assets/github-logo.png" alt="github"/></a>
-          <a href="https://www.linkedin.com/in/rafael-hernandez-soler-7a449323b/" target="_blank"><img src="../assets/linkedin-logo.png" alt="linkedin"/></a>
+        <div id="footer-img">
+          <a href="https://github.com/Rafa3399" target="_blank"><img id="github" src={githubLogo} alt="github"/></a>
+          <a href="https://www.linkedin.com/in/rafael-hernandez-soler-7a449323b/" target="_blank"><img id="linkedin" src={linkedinLogo} alt="linkedin"/></a>
+        </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,20 @@
-import React from "react";
 import "./Navbar.css";
-
+import Logo from "../assets/Logo.png";
 
 function Navbar() {
-
-
     return (
-<div className="Navbar">
-<div className="logo">
-<img src="" />
-</div>
-<div className="nav-links">
-<a href="#home">Home</a>
-<a href="#about">About</a>
-<a href="#services">Services</a>
-<a href="#contact">Contact</a>
-</div>
-</div>
-    )
-
+        <div className="navbar">
+            <div className="logo">
+                <img src={Logo} alt="Logo" width="200px" height="200px" />
+            </div>
+            <div className="nav-links">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#services">Services</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
